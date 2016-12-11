06316c484ccf00597d051724e751a4f14e521003
Axel Banal <axel.banal@epita.fr>
2016-12-11 03:35:30 +0100

    [fix] fixed bugs on camera, added Search and profile implementations, sending winds is working but it's manually set to send one to the user 77.\n Problem with parsing server response on users array

---
+++ app/src/main/java/com/app/windchat/ui/adapter/list/FriendListRecyclerAdapter.java
@@ -0,0 +1,8 @@
+package com.app.windchat.ui.adapter.list;
+
+/**
+ * Created by banal_a on 10/12/2016.
+ */
+
+public class FriendListRecyclerAdapter {
+}
---
+++ app/src/main/java/com/app/windchat/ui/view/FriendListViewHolder.java
@@ -0,0 +1,8 @@
+package com.app.windchat.ui.view;
+
+/**
+ * Created by banal_a on 10/12/2016.
+ */
+
+public class FriendListViewHolder {
+}
